36738d50d845f602024d6d2a752d509f31787358
Reinoptland <reinoptland@gmail.com>
2020-04-14 13:08:22 +0200

    Fetch todos in componentDidMount

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 
 export default class TodoList extends Component {
+  async componentDidMount() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const todos = await response.json();
+
+    console.log(todos);
+  }
   render() {
     return <div>Todolist</div>;
   }
