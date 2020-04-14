c070f581787c2b44e08aa8e2c38bebb4a4d1fd08
Reinoptland <reinoptland@gmail.com>
2020-04-14 13:11:09 +0200

    Store todos in state after fetching them

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
 
 export default class TodoList extends Component {
+  state = {
+    todos: [],
+  };
   async componentDidMount() {
     const response = await fetch("https://jsonplaceholder.typicode.com/todos");
     const todos = await response.json();
 
     console.log(todos);
+    this.setState({ todos: [...this.state.todos, ...todos] });
   }
   render() {
-    return <div>Todolist</div>;
+    console.log(this.state);
+    return (
+      <div>
+        {this.state.todos.map((todo) => {
+          return <li key={todo.id}>{todo.title}</li>;
+        })}
+      </div>
+    );
   }
 }
